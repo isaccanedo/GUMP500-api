4988d0a53cc6110d148d92d226471ac465a69c91
isaccanedo <isaccanedo@gmail.com>
2022-07-26 10:34:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GUMP500
+
+GUMP500 is a Twitter bot that tracks people's achievements in the Forrest Gump 500 challenge. The bot scours for tweets containing the `#Gump500` and a number followed by `mi`, `mile(s)`, `km` or `kilometer(s)`. It collects the data and displays it on a webpage.
+
+### Set up your local environment
+
+1. Copy the env-sample file into a `.env` file
+2. Run `npm install`
+1. The project uses MongoDB as a database. To develop and test locally, you'll need one.
+   The easiest solution is to register a free account on [MongoDB](https://www.mongodb.com/) and start a free cloud server (M0 shared cluster).
+   Once you've done that, all you need to do is change the `MONGO_CONNECTION_URI` variable in the `.env` file
+1. You need to have a Twitter API Key to be able to fetch the tweets with hashtag `#GUMP500` (if you want to test this functionality). In order to get this, you first need an [approved developer account](https://developer.twitter.com/en/portal/dashboard).
+   Once you have that, just create a new app, generate your tokens, put them in the `.env` file, and off you go!
+
+
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+const { MongoClient } = require("mongodb");
+
+console.log("hello GUMP500! 🤖");
+
+// Get environment variables from .env for Twitter/MongoDB creds
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_CONNECTION_URI);
+let runnersDB;
+
+connectDB().catch(console.error);
+
+async function connectDB() {
+  try {
+    // Connect to the MongoDB cluster
+    await mongoClient.connect();
+    // Make the appropriate DB calls
+    databasesList = await mongoClient.db().admin().listDatabases();
+    databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+    runnersDB = mongoClient.db("GUMP500").collection("people");
+    const all = await getRunners();
+    console.log(`There are ${all.length} database entries`);
+  } catch (e) {
+    console.error(`MongoDB connection error: ${e}`);
+  } finally {
+    // await mongoClient.close();
+  }
+}
+
+// Return all runners from datbase as an array
+async function getRunners() {
+  // await mongoClient.connect();
+  // runnersDB = mongoClient.db("GUMP500").collection("people");
+  const cursor = runnersDB.find();
+  const all = await cursor.toArray();
+  return all;
+}
+
+// Create Express web application at port 3000
+const app = express();
+app.listen(3000, () => console.log("listening at 3000"));
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+app.use(cors());
+
+// Return all runners as json on /api web request
+app.get("/api", async (request, response) => {
+  // const cursor = runnersDB.find();
+  // const all = await cursor.toArray();
+  const all = await getRunners();
+  // console.log(all);
+  response.json(all);
+});
